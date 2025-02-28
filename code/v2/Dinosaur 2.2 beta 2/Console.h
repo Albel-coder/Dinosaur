710f5e368eb702087f258f9ff88f94387de5cbe0
Aleksey <alekseybel2505@gmail.com>
2025-02-28 23:58:14 +0300

    class Console

---
+++ code/v2/Dinosaur 2.2 beta 2/Console.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <Windows.h>
+
+struct Console
+{
+	unsigned char MinConsoleWidth, MinConsoleHeight;
+
+	Console();
+
+	void GetBuffer();
+
+	short& GetConsoleWidth();
+
+	short& GetConsoleHeight();
+
+	bool IsChanged();
+
+	bool Acceptable();	
+
+	void ConsoleCursorVisible(bool show, short size);
+
+private:
+
+	short ConsoleWidth, ConsoleHeight, CharsX, CharsY;
+
+
+	CONSOLE_SCREEN_BUFFER_INFO csbInfo;
+};
